a2bce6fe3194dcef472b3858a17081aff88906a2
vinodwagh07 <vinodwagh.rcpit2003@gmail.com>
2025-11-25 18:34:55 +0530

    feat(rootReducer): add profile and cart slices

--- client/src/app/rootReducer.js
+++ client/src/app/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import profileReducer from "../features/profile/profileSlice";
+import cartReducer from "../features/cart/cartSlice";
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  profile: profileReducer,
+  cart: cartReducer,
 });
 
 export default rootReducer;
