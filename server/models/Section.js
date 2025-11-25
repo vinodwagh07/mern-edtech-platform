28ec26aebc70e5cd68b09062cb982ded0b427eb2
vinodwagh07 <vinodwagh.rcpit2003@gmail.com>
2025-11-25 11:37:59 +0530

    feat(section): add courseId reference to parent course

--- server/models/Section.js
+++ server/models/Section.js
@@ -1,17 +1,25 @@
 const mongoose = require("mongoose");
 
-const SectionSchema = new mongoose.Schema({
-  sectionName: {
-    type: String,
-    required:true,
-    trim:true,
-  },
-  subSections: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "SubSection",
+const SectionSchema = new mongoose.Schema(
+  {
+    sectionName: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  ],
-});
+    subSections: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubSection",
+      },
+    ],
+    courseId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    }, // parent course
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Section", SectionSchema);
